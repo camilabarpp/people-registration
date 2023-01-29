7b2f48ed83c899ee749a0daaaa5a409181dbf78e
Camila Barpp <milabarpp5.com>
2023-01-29 08:34:07 -0300

    Testes com address service e controller concluido e estão passando

--- src/test/java/camila/peopleregistration/controller/AddressControllerTest.java
+++ src/test/java/camila/peopleregistration/controller/AddressControllerTest.java
@@ -2,9 +2,12 @@ package camila.peopleregistration.controller;
 
 import camila.peopleregistration.configuration.exception.NotFoundException;
 import camila.peopleregistration.model.address.entity.AddressEntity;
+import camila.peopleregistration.repository.AddressRepository;
 import camila.peopleregistration.service.AddressService;
 import camila.peopleregistration.service.stubs.AddressStubs;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,7 +20,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.*;
@@ -35,7 +37,26 @@ class AddressControllerTest {
     @MockBean
     private AddressService service;
 
-    private final String url = "http://localhost:8080/v1/person/1/address";
+    @MockBean
+    private final AddressRepository repository;
+
+
+    private String url = "http://localhost:8080/v1/person/{personId}/address/";
+
+    @BeforeEach
+    void setUp() {
+        this.url = "http://localhost:8080/v1/person/1/address";
+    }
+
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll();
+    }
+
+    @Autowired
+    AddressControllerTest(AddressRepository repository) {
+        this.repository = repository;
+    }
 
     @Test
     @DisplayName("Deve retornar uma lista de endereços quando buscar por id de pessoa válido")
@@ -47,7 +68,7 @@ class AddressControllerTest {
         );
         when(service.getAddressesByPersonId(personId)).thenReturn(addresses);
 
-        mvc.perform(get(url))
+        mvc.perform(get(url, personId))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
                 .andExpect(jsonPath("$", hasSize(2)));
@@ -62,7 +83,7 @@ class AddressControllerTest {
 
         when(service.getAddressesByPersonId(personId)).thenThrow(new NotFoundException("Address not found"));
 
-        mvc.perform(get(url))
+        mvc.perform(get(url, personId))
                 .andExpect(status().isNotFound())
                 .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
                 .andExpect(jsonPath("$.timestamp").exists())
@@ -84,10 +105,9 @@ class AddressControllerTest {
         ObjectMapper mapper = new ObjectMapper();
 
         String json = mapper.writeValueAsString(address);
-        mvc.perform(post(url)
+        mvc.perform(post(url, personId)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(json)
-                        .param("personId", personId.toString()))
+                        .content(json))
                 .andExpect(status().isCreated());
 
         verify(service, times(1)).createNewAddress(address, personId);
@@ -104,10 +124,9 @@ class AddressControllerTest {
         ObjectMapper mapper = new ObjectMapper();
 
         String json = mapper.writeValueAsString(address);
-        mvc.perform(post(url)
+        mvc.perform(post(url, personId)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(json)
-                        .param("personId", personId.toString()))
+                        .content(json))
                 .andExpect(status().isNotFound())
                 .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
                 .andExpect(jsonPath("$.timestamp").exists())
@@ -131,7 +150,7 @@ class AddressControllerTest {
         ObjectMapper mapper = new ObjectMapper();
 
         String json = mapper.writeValueAsString(addressResponse);
-        mvc.perform(put("http://localhost:8080/v1/person/{personId}/address/{addressId}", personId, addressId)
+        mvc.perform(put(url + "/{addressId}", personId, addressId)
                 .contentType(APPLICATION_JSON)
                 .content(json)
         ).andExpect(status().isCreated());
@@ -153,7 +172,7 @@ class AddressControllerTest {
         ObjectMapper mapper = new ObjectMapper();
 
         String json = mapper.writeValueAsString(addressExpect);
-        mvc.perform(put("http://localhost:8080/v1/person/{personId}/address/{addressId}", personId, addressId)
+        mvc.perform(put(url + "/{addressId}", personId, addressId)
                 .contentType(APPLICATION_JSON)
                 .content(json))
                 .andExpect(status().isNotFound())
@@ -167,22 +186,36 @@ class AddressControllerTest {
     @Test
     @DisplayName("Deve deletar um endereço quando passar um id de pessoa válido")
     void deleteAddressByPersonId_shouldReturnCreated() throws Exception {
-//        var addressExpect = AddressStubs.createAddress();
-//        var addressResponse = AddressStubs.createAddress();
-//        Long personId = 1L;
-//        Long addressId = 1L;
-//
-//        when(service.deleteAddressByPersonId(personId, addressId));
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//
-//        String json = mapper.writeValueAsString(addressResponse);
-//        mvc.perform(put("http://localhost:8080/v1/person/{personId}/address/{addressId}", personId, addressId)
-//                .contentType(APPLICATION_JSON)
-//                .content(json)
-//        ).andExpect(status().isCreated());
-//
-//        verify(service, times(1)).updateAddressByPersonId(addressExpect, personId, addressId);
+        Long personId = 1L;
+        Long addressId = 1L;
+
+        doNothing().when(service).deleteAddressByPersonId(personId, addressId);
+
+        mvc.perform(delete(url + "/{addressId}", personId, addressId)
+                .contentType(APPLICATION_JSON)
+        ).andExpect(status().isNoContent());
+
+        verify(service, times(1)).deleteAddressByPersonId(personId, addressId);
     }
 
+    @Test
+    @DisplayName("Deve retornar erro 404 quando tentar deletar com um id de pessoa inválido")
+    void deleteAddressByPersonId_shouldReturnNotFound() throws Exception {
+        Long personId = 1L;
+        Long addressId = 1L;
+
+        doThrow(new NotFoundException("Error to delete a address, please check your data!"))
+                .when(service)
+                .deleteAddressByPersonId(personId, addressId);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        mvc.perform(delete(url + "/{addressId}", personId, addressId))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
+                .andExpect(jsonPath("$.timestamp").exists())
+                .andExpect(jsonPath("$.message").value("Error to delete a address, please check your data!"))
+                .andExpect(jsonPath("$.field").value("NOT_FOUND"))
+                .andExpect(jsonPath("$.parameter").value("NotFoundException"));
+    }
 }
